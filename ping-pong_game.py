87a28860f9cb80ae183cf82fcaef8da793064e16
qwadracickl <167782579+qwadracickl@users.noreply.github.com>
2024-05-02 16:38:14 +0300

    Add files via upload
    на данный момент закончена разработка смены цвета фона!!! Фон сменяется командой в консоли python. Сейчас занимаюсь разработкой спрайтов и их движением.

--- ping-pong_game.py
+++ ping-pong_game.py
@@ -8,18 +8,35 @@ class RGB_Change():
         self.red = red
         self.green = green
         self.blue = blue
+
     def change(self):
-        color_change = int(input('выберите цвет который вы будете менять(1, 2, 3: '))
-        if color_change == 1:
-            color_red_change = int(input('выберите размерц цвета от 0 до 255: '))
-            if color_change > 255:
-                self.red = 255
-            else:
-                self.red = color_red_change
+        color_change = int(input('выберите цвет который вы будете менять(1, 2, 3) 0 - выйти'))
+        while color_change != 0:
+            if color_change == 1:
+                color_red_change = int(input('выберите размерц цвета от 0 до 255: '))
+                if color_red_change > 255:
+                    self.red = 255
+                else:
+                    self.red = color_red_change
+            if color_change == 2:
+                color_red_change = int(input('выберите размер цвета от 0 до 255: '))
+                if color_red_change > 255:
+                    self.green = 255
+                else:
+                    self.green = color_red_change
+            if color_change == 3:
+                color_red_change = int(input('выберите размерц цвета от 0 до 255: '))
+                if color_red_change > 255:
+                    self.blue = 255
+                else:
+                    self.blue = color_red_change
+            color_change = int(input('выберите цвет который вы будете менять(1, 2, 3) 0 - выйти'))
+        return (self.red, self.green, self.blue)
+        
 
-
-back = (255, 255, 255)
+object1 = RGB_Change(0, 0, 0)
 window = display.set_mode((win_x, win_y))
+back = (object1.change())
 window.fill(back)
 
 clock = time.Clock()
