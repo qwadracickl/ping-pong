df242c808739390f76dc8514af92d229bca43c06
qwadracickl <167782579+qwadracickl@users.noreply.github.com>
2024-05-02 17:01:00 +0300

    Add files via upload
    Нашёл спрайты мяча и ракетки. Написал класс GameSprite и готовлю класс Player для ракеток.

--- ping-pong_game.py
+++ ping-pong_game.py
@@ -3,6 +3,21 @@ from pygame import *
 win_x = 600
 win_y = 500
 
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+#сдесь будет класс player
+
 class RGB_Change():
     def __init__(self, red, green, blue):
         self.red = red
